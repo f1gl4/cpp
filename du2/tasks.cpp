720767cb39a04a13b9718e28511a8a4c76225e57
f1gl4 <runcatkit@gmail.com>
2025-03-07 23:18:10 +0100

    1 part done

--- du2/tasks.cpp
+++ du2/tasks.cpp
@@ -1,1 +1,39 @@
 #include "tasks.h"
+
+
+//UTF8String(const std::string& str); // constructor string
+
+//UTF8String(const char* str);
+
+//UTF8String(const std::vector<CodePoint>& codepoints); // constructor codepoints vector
+
+UTF8String::UTF8String(const UTF8String& other)
+{
+    this->size_ = other.size_;
+    this->capacity_ = other.capacity_;
+    this->data_ = new uint8_t[this->capacity_];
+    std::copy(other.data_, other.data_ + other.size_, this->data_);
+}
+
+//UTF8String::UTF8String& operator=(const UTF8String& other); // copy assignm
+
+UTF8String::~UTF8String()
+{
+    delete[] this->data_;
+}
+
+bool UTF8String::operator==(const UTF8String &other) const
+{
+    return std::equal(this->data_, this->data_ + this->size_, other.data_, other.data_ + other.size_);
+}
+
+UTF8String::operator std::string() const
+{
+    return std::string(reinterpret_cast<const char*>(this->data_), this->size_);
+}
+
+
+std::size_t UTF8String::get_byte_count() const 
+{
+    return this->size_;
+}
--- du2/tasks.h
+++ du2/tasks.h
@@ -7,6 +7,39 @@
 #include <utility>
 #include <cstring>
 
+#include <optional>
+
 using CodePoint = uint32_t;
 
-class UTF8String {};
+class UTF8String {
+public:
+    UTF8String(); // constructor
+    UTF8String(const std::string& str); // constructor string
+    UTF8String(const char* str);
+    UTF8String(const std::vector<CodePoint>& codepoints); // constructor codepoints vector
+    UTF8String(const UTF8String& other); // copy constructor
+    UTF8String& operator=(const UTF8String& other); // copy assignm
+
+    // operators
+    UTF8String operator+(const UTF8String& other) const; // concatenate (new object return)
+    UTF8String& operator+=(const UTF8String& other); // append (change cur object)
+    bool operator==(const UTF8String &other) const; // equal
+    operator std::string() const; // converted to std::string by explicit cast
+
+    void append(char ch); // add new ascii chars
+    void append(CodePoint codepointer); // new code points
+
+    std::optional<CodePoint> ntn_code_point(int index) const; // indexed by code point
+    std::optional<uint8_t> operator[](int index) const; // indexed by bytes
+
+    std::size_t get_byte_count() const; // cur stored bytes
+    std::size_t get_point_count() const; // cur stored code points
+
+
+    ~UTF8String(); // destructor
+
+private:
+    uint8_t* data_;
+    std::size_t size_;
+    std::size_t capacity_;
+};
